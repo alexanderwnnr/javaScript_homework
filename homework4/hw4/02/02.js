6893c75547d8b94912fa1d5c8b083c2c373bda75
alexanderwnnr <alexander.wnnr@gmail.com>
2019-12-11 14:56:31 +0200

    hw4

---
+++ homework4/hw4/02/02.js
@@ -0,0 +1,8 @@
+var Person = function (name, surname){
+    this.name = "name";
+    this.surame = "surname"
+}
+var a = new Person(), b = new Person(), c = new Person();
+a.fatherName = "fatherName";
+b.age = "age";
+c.sex = "sex"
---
+++ homework4/hw4part2/01/01.js
@@ -0,0 +1,10 @@
+var obj = {
+	element1: 'element1',
+    element2: 2,
+	deleteElement (key) {
+	    delete obj[key]
+    },
+    addElement (key, value) {
+	    obj[key] = value
+    }
+}
